4986d67b5e797ae999b44f31af0790223bf09b22
Daniel Dias <daniel.dias.g@gmail.com>
2013-10-15 19:11:15 +0100

    input

--- wolves-squirrels-serial.h
+++ wolves-squirrels-serial.h
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 #define MAX 50
 
 enum entity {
@@ -10,7 +14,7 @@ enum entity {
 typedef enum entity entity_types;
 
 struct world {
-	int type; /* Wolf, Squirrel, etc. */
+	entity_types type; /* Wolf, Squirrel, etc. */
 	int breeding_period;
 	int starvation_period;
 } world[MAX][MAX];
