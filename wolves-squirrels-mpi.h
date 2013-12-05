087bb79f99c8366d10a98c94c4d0c49913d75e8f
Daniel Dias <daniel.dias.g@gmail.com>
2013-12-05 16:29:42 +0000

    MPI

--- wolves-squirrels-mpi.h
+++ wolves-squirrels-mpi.h
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define MAX 1000
+
+#define MASTER 0       /* id of the first process */ 
+
+clock_t start;
+clock_t end;
 
 enum entity {
 	wolf='w',
@@ -12,25 +18,28 @@ enum entity {
 	tree='t',
 	empty=' '
 };
-
 typedef enum entity entity_types;
 
 struct point {
 	int x;
 	int y;
 };
-struct point {
 
+struct conflicts {
+	entity_types type;
+	int breeding_period;
+	int starvation_period;
+};
+typedef struct conflicts conflict; 
 
 struct world {
-	struct point coord;
+	conflict *conflicts[5];
+	int count;
 	entity_types type; /* Wolf, Squirrel, etc. */
 	int breeding_period;
 	int starvation_period;
-	int breed; /* indicates if the entity can breed */
-} world[2][MAX][MAX];
-
-int w_number;
+	int breed;  /*indicates if the entity can breed */
+} world[MAX][MAX];
 
 /* Fill all the cells, of the square matrix of size world_size, with the empty entity_types*/
 int initWorld(int world_size);
@@ -38,28 +47,13 @@ int initWorld(int world_size);
 /* Prints the content of the world, which corresponds to a square matriz of size world_size */
 int printWorld(int world_size);
 
-/* Copies the contents of the real world to the fake world */
-int copyWorldTo(struct world real_world[MAX][MAX], struct world fake_world[MAX][MAX]);
-
 /* Prints the contents of the world, according to the expected output format, which must correspond
  * to the input file format. */
 int printWorldFormatted(int world_size);
 
-/* Corrects the world, according to the performed move */
-int correctWorld(entity_types type, int prev_x, int prev_y, int curr_x, int curr_y, int world_size);
-
-/* Clears a specified cell in the world */
-int clearWorldCell(struct world* cell);
-
-int killWolf(struct world* wolf);
-
 /* Processes the cell's iteration, depending of what kind of entity is at the current cell
  * specified by the position (x, y) */
 int computeCell(int x, int y, int s_breeding, int w_breeding, int w_starvation, int world_size);
-
-/* Given the entity previous position and it's current position, creates an heir at the previous position
-   with the specified breeding and starvation periods, and restarts the entity's breeding period */
-int makeBabies(entity_types type, struct world* prev_cell, struct world* curr_cell, int breeding_period, int starvation_period);
 
 /**/
 struct world *move(entity_types e, int x, int y, int size);
